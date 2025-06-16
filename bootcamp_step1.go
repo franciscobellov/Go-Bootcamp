4ebcbfe0b483dbfbafb053f87db0900df40c74fe
franciscobellov <franciscobello23@outlook.com>
2025-06-16 11:56:05 -0300

    Actualizacion go.sum

---
+++ bootcamp_step1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Este es un programa básico en Go.
+	// Imprime un mensaje en la consola.
+	// Muy simple de aprender.
+	fmt.Println("welcome to the bootcamp")
+	// Puedes ejecutar este programa con el comando: go run bootcamp.go
+}
