c31ec284d6bd4e74c004ed34b146c1bbfe7fee89
franciscobellov <franciscobello23@outlook.com>
2025-06-17 00:00:35 -0300

    cambios

---
+++ 02-variables-data.md.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func variablesData() {
+
+	// Variables
+	var Nombre string
+	var Edad int
+
+	// Asignación de valores
+
+	Nombre = "Francisco"
+	Edad = 30
+
+	// Imprimir variables
+	fmt.Println(Nombre)
+	fmt.Println(Edad)
+}
